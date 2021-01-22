28305816ee96e4cc1f51bc373ca6151e33ffc59b
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2021-01-22 22:41:21 +0500

    Added code for using intvector library

---
+++ src/use_intvector.cc
@@ -0,0 +1,15 @@
+#include "libintvector.cc"
+
+
+int main() {
+    IntVector vec1 = IntVector();
+    vec1.appendInt(4);
+    vec1.appendInt(5);
+
+    vec1.Print();
+
+    IntVector vec2(std::move(vec1));
+    vec2.Print();
+
+    vec1.Print();
+}
