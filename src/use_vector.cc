edac9f4268fe8cf35b7fd11f86cfd92ff4567fea
syedmohsinbukhari <syedmohsinbukhari@googlemail.com>
2021-01-24 02:40:10 +0500

    Using CMAKE_BUILD_TYPE for checking in code Release/Debug

--- src/use_vector.cc
+++ src/use_vector.cc
@@ -3,6 +3,12 @@
 
 
 int main() {
+#if defined(DEBUG)
+    std::cout << "Debug mode!" << std::endl;
+#endif
+#if defined(RELEASE)
+    std::cout << "Release mode!" << std::endl;
+#endif
     Vector<double> vec1 = Vector<double>();
     vec1.append(4.5);
     vec1.append(5.4);
