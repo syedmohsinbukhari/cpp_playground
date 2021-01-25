7cdd44a3fbf836678c79ede847114b00c0ccbd53
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2021-01-25 20:37:38 +0500

    Added an istream example code

---
+++ src/use_istream.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+int main() {
+    std::string inp1;
+    std::string inp2;
+    int inp3;
+
+    std::cin >> inp1 >> inp2 >> inp3;
+
+    std::cout << inp1 << std::endl << inp2 << std::endl << inp3 << std::endl;
+
+    return 0;
+}
